f242d8c3bae5bfd280adeb55143af70c88aa1fba
Xavier <x.crochet@gmail.com>
2020-07-22 17:26:26 +0200

    feat (structure) Add structure site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SiteComponent } from './pages/site/site.component';
+import { PortfolioComponent } from './pages/site/portfolio/portfolio.component';
+import { BlogComponent } from './pages/site/blog/blog.component';
+import { HeaderComponent } from './pages/site/shared/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SiteComponent,
+    PortfolioComponent,
+    BlogComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
